3fdd1c8c670d2ee920b0f6a83d98e90de7afa6eb
Bmbus <www.bmbus@gmail.com>
2019-03-28 17:17:36 +0100

    Add: "install_python_dpy.sh"

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# UtilityScripts
-Some utilty scripts
+div align=center>
+    <h1>~ UtilityScripts ~</h1>
+    <strong>Some utility scripts for my workflow lul</strong> </br>
+    </br>
+</div>
+
+<div align=center>
+    <strong>by Bmbus© (2019)</strong>
+</div>
---
+++ raspberry/install_python_dpy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# check if python is installed
+if command -v python3 &>/dev/null; then
+    install_dpy
+else
+    install_python
+fi
+
+function install_python() {
+    # install python
+    sudo apt-get update -y
+    sudo apt-get upgrade
+    sudo apt-get dist-upgrade
+    sudo apt-get install libssl-dev -y
+    sudo apt-get install libffi-dev -y
+    sudp apt-get install libsqlite3-dev
+    wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+    tar xzvf Python-3.7.2.tgz
+    cd Python-3.7.2/
+    ./configure
+    make
+    sudo make install
+}
+
+function install_dpy() {
+    # install discord.py rewrite
+    pip3 install git+https://github.com/Rapptz/discord.py@rewrite#egg=discord.py
+}
+
